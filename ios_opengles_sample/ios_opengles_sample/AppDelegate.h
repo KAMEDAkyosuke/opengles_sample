c6459bbc26c0c87e9c3a5ef351d7ffe979500d9c
KAMEDAkyosuke <kameda-kyosuke@kayac.com>
2014-06-28 16:46:54 +0900

    init repo

---
+++ ios_opengles_sample/ios_opengles_sample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios_opengles_sample
+//
+//  Created by organlounge on 2014/06/28.
+//  Copyright (c) 2014年 KAMEDAkyosuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios_opengles_sample/ios_opengles_sample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ios_opengles_sample
+//
+//  Created by organlounge on 2014/06/28.
+//  Copyright (c) 2014年 KAMEDAkyosuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
